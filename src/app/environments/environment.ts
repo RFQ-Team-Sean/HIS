807c566245a287f5bc70d4af7ff72545feaf6e90
JunLomerio <ferdinand.lomeriojr@gmail.com>
2025-01-15 14:17:16 +0800

    updated API to new supabase

--- src/app/environments/environment.ts
+++ src/app/environments/environment.ts
@@ -1,6 +1,6 @@
 export const environment = {
     production: false,
-    supabaseUrl: 'https://vhmftufkipgbxmcimeuq.supabase.co',
-    supabaseKey: 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6InZobWZ0dWZraXBnYnhtY2ltZXVxIiwicm9sZSI6ImFub24iLCJpYXQiOjE3MTk5MDExODAsImV4cCI6MjAzNTQ3NzE4MH0.7bzTx5n4SpXA1Go9kCRfgsxUIpK8j68vM-hIpVKJcnw'
+    supabaseUrl: 'https://uerxuiyzostglforelsg.supabase.co',
+    supabaseKey: 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6InVlcnh1aXl6b3N0Z2xmb3JlbHNnIiwicm9sZSI6ImFub24iLCJpYXQiOjE3MzYyNDc3ODQsImV4cCI6MjA1MTgyMzc4NH0.2vuCtxdYnHSfp5kdNbiGUC03h5VX8Zqmeq8jvSNXC_E'
   };
   
