6f0b82981ec7a1b3f13da99ffbdd5b9bbf60559c
Kenrix Marquez <kenrixmarquez@gmail.com>
2025-01-28 13:17:53 +0800

    added shared header component

--- src/app/environments/environment.ts
+++ src/app/environments/environment.ts
@@ -2,4 +2,4 @@ export const environment = {
     production: false,
     supabaseUrl: 'https://uerxuiyzostglforelsg.supabase.co',
     supabaseKey: 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6InVlcnh1aXl6b3N0Z2xmb3JlbHNnIiwicm9sZSI6ImFub24iLCJpYXQiOjE3MzYyNDc3ODQsImV4cCI6MjA1MTgyMzc4NH0.2vuCtxdYnHSfp5kdNbiGUC03h5VX8Zqmeq8jvSNXC_E'
-  };
+   };
--- src/app/shared/layout/layout.component.ts
+++ src/app/shared/layout/layout.component.ts
@@ -1,10 +1,11 @@
 import { Component } from '@angular/core';
 import { SidebarComponent } from '../sidebar/sidebar.component';
 import { RouterOutlet } from '@angular/router';
+import { HeaderComponent } from '../header/header.component';
 @Component({
   selector: 'app-layout',
   standalone: true,
-  imports: [SidebarComponent,RouterOutlet],
+  imports: [SidebarComponent,RouterOutlet, HeaderComponent],
   templateUrl: './layout.component.html',
   styleUrl: './layout.component.css'
 })
